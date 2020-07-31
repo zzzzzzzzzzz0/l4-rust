c0030d7f04879421cadec4dd96a5e8f5fa6fce32
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2020-08-01 06:09:31 +0800

    改

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,90 @@
-#![feature(proc_macro_hygiene, asm)]
+use zhscript2::{u_::*, as_ref__, as_mut_ref__};
+use zs2_l4_;
+use std::os::raw::c_char;
+use std::ffi::{CStr, CString};
 
-use zhscript2::{as_ref__, as_mut_ref__};
 
-mod dl_;
-mod func_;
-mod call_;
-mod funcs_;
-mod l4_;
-mod i_;
+#[no_mangle]
+pub extern "C" fn i2_free__(s:*mut c_char) {
+	if !s.is_null() {
+		unsafe {
+			CString::from_raw(s);
+		}
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn i2__(s:*const c_char, c:u8) -> *mut c_char {
+	let mut s2 = String::new();
+	unsafe {
+		let s = CStr::from_ptr(s);
+		if let Ok(s) = s.to_str() {
+			s2.push_str(s)
+		}
+	}
+	if !s2.is_empty() {
+		let w = zs2_l4_::i_::w__();
+		let w2 = as_ref__!(w);
+		let mut wm:WorldMut_ = Default::default();
+		let  q = Qv_::new2(Some(w2.clone().top_q_));
+		let mut ret2 = result_::List_::new();
+		let ret3 = eval_::hello__(&s2, &code_::Env_::new(qv_::t__(q), w.clone()), &mut wm, &mut ret2);
+		s2.clear();
+		if ret3.is_ok() {
+			match c {
+				b'j' | b'1' => ret2.s2__(c, &mut s2),
+				_ => {}
+			}
+		} else {
+			println!("{:?} {:?}", ret3, ret2);
+		}
+	}
+	CString::new(s2).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub extern fn i__(l4_so:&str, a:&[String], ret:&mut Vec<String>) -> i32 {
+	let w = zs2_l4_::i_::w__();
+	{
+		let w = as_mut_ref__!(w);
+		let mut top_q = as_mut_ref__!(w.top_q_);
+		top_q.val__("l4.so", l4_so);
+	}	
+
+	let mut v = vec![];
+	for i in a {
+		v.push(i.to_string())
+	}
+	
+	let mut wm:WorldMut_ = Default::default();
+	let w2 = as_ref__!(w);
+	let mut q = Qv_::new2(Some(w2.clone().top_q_));
+	let mut ret2 = result_::List_::new();
+	let ret3 = w2.clone().hello3__(&mut v.into_iter(), true, true, false, &mut q, &mut wm, &mut ret2);
+	*ret = ret2.to_vec__();
+	w2.clone().ret__(ret3)
+}
+
+#[no_mangle]
+extern fn clpars4_set__(env:&code_::Env_, _wm:&mut WorldMut_, ret:&mut result_::List_) -> Result2_ {
+	zs2_l4_::clpars4_::set__(&as_ref__!(env.q).args_.to_vec__(), ret)
+}
+
+#[no_mangle]
+extern fn clpars4_par__(env:&code_::Env_, wm:&mut WorldMut_, ret:&mut result_::List_) -> Result2_ {
+	match zs2_l4_::clpars4_::par__(0, 1, env, wm, ret) {
+		Ok(()) => ok__(),
+		Err((i, ret2)) => {
+			match i {
+				251 => result2_::n__(jump_::QUIT_),
+				_ => ret2
+			}
+		}
+	}
+}
+
+#[no_mangle]
+extern fn clpars4_help__(env:&code_::Env_, _wm:&mut WorldMut_, ret:&mut result_::List_) -> Result2_ {
+	zs2_l4_::clpars4_::help__(0, env.q.clone(), ret);
+	ok__()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::{env, path::{Path}, process};
 
-type I_ = fn(a:&[String], ret:&mut Vec<String>) -> i32;
+type I_ = fn(l4_so:&str, a:&[String], ret:&mut Vec<String>) -> i32;
 
 fn main() {
 	let a: Vec<String> = env::args().collect();
-	let path = &a[0];
-	let path = Path::new(&path);
-	let mut path = path.canonicalize().unwrap();
-	path.set_file_name("l4.so");
-	if !path.exists() {
-		path.set_file_name("libl4.so");
+	let mut path;
+	{
+		let path1 = Path::new(&a[0]);
+		path = path1.canonicalize().unwrap();
+		path.set_file_name("l4.so");
+		if !path.exists() {
+			path.set_file_name("libl4.so");
+		}
 	}
-	let lib = libloading::Library::new(path);
+	let lib = libloading::Library::new(&path);
 	let lib = match lib {
 		Ok(lib) => lib,
 		Err(e) => {
@@ -29,9 +31,8 @@ fn main() {
 			}
 		}
 	}
-	//println!("{:?}", a);
 	let mut ret2 = vec![];
-	let ret = i__(&a, &mut ret2);
+	let ret = i__(path.to_str().unwrap(), &a, &mut ret2);
 	//println!("{} {:?}", ret, ret2);
 	process::exit(ret);
 }
